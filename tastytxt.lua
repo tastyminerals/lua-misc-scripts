90fc110c24078f7a233f13f93c10c80849cf8676
tastyminerals <tastyminerals@gmail.com>
2017-01-09 00:28:26 +0100

    added couple of new functions, fixed some bad bugs

--- tastytxt.lua
+++ tastytxt.lua
@@ -1,6 +1,8 @@
 --[[A small collection of lua snippets to work with multilingual text]]
+-- to force reload the module use: package.loaded.tastytxt = nil; require 'tastytxt'
 local tastytxt = {}
 
+pl = require 'pl'
 utf8 = require 'lua-utf8'
 --tds = require 'tds' --https://github.com/torch/tds
 --pretty = require 'pl.pretty'
@@ -12,7 +14,7 @@ function tastytxt.ls(path)
     for fname in pfile:lines() do
         i = i + 1
         -- path must end on / or \
-        fpath = path..fname
+        fpath = path..'/'..fname
         files[i] = fpath
     end
     pfile:close()
@@ -28,9 +30,16 @@ function tastytxt.read(fname)
   return fdata
 end
 
+-- write text to file, if table is given, concatenate into text
 function tastytxt.write(text,fname)
   local text = text
   local ofile = fname
+
+  -- handle table
+  if type(text) == "table" then
+    text = table.concat(text, '\n')
+  end
+
   -- write file
   ofile = assert(io.open(fname,'w'))
   ofile:write(text)
@@ -47,8 +56,7 @@ end
 -- separate punctuation from text
 function tastytxt.tokpunct(text)
   local text = text
-  local new_text = ''
-  local loops = 0
+  local lns = {}
   -- iterate over each line, otherwise we are breaking text formatting
   for line in utf8.gmatch(text,"[^\n]+") do
     -- surround punctuation with spaces
@@ -57,35 +65,39 @@ function tastytxt.tokpunct(text)
     line = utf8.gsub(line,"%s%s+"," ")
     -- fix 's
     line = utf8.gsub(line," ' s "," 's ") -- for English
-    new_text = new_text..line.."\n"
-    loops = loops + 1
-    if loops % 10000 == 0 then collectgarbage() end
+    table.insert(lns,line)
   end
-  return new_text
+  return table.concat(lns,'\n')
 end
 
 -- count the number of tokens using str match
-function tastytxt.tokcnt(text)
+function tastytxt.cnt(text)
   local text = text
-  local _,cnt = utf8.gsub(text,"%S+","")
+  -- separate punctuation from words first
+  local _,cnt = utf8.gsub(tastytxt.tokpunct(text),"%S+",'')
+  return cnt
+end
+
+-- count only punctuation chars
+function tastytxt.cntpnc(text)
+  local text = text
+  -- separate punctuation from words first
+  local _,cnt = utf8.gsub(tastytxt.tokpunct(text),"%p",'')
   return cnt
 end
 
 -- strip punctuation
 function tastytxt.stripp(text)
   local text = text
-  local new_text = ''
-  local loops = 0
+  local lns = {}
   -- iterate over each line, otherwise we are breaking text formatting
   for line in utf8.gmatch(text,"[^\n]+") do
     line = utf8.gsub(line,"%p",' ')
     -- replace mult space with single space
     line = utf8.gsub(line,"%s%s+"," ")
-    new_text = new_text..line.."\n"
-    loops = loops + 1
-    if loops % 10000 == 0 then collectgarbage() end
+    table.insert(lns,line)
   end
-  return new_text
+  return table.concat(lns,'\n')
 end
 
 -- refit corpus so that each sentence takes one line
@@ -103,7 +115,7 @@ end
 -- tokenize text and return lines array of tokens array
 function tastytxt.tok(text)
   local text = text
-  local lines = {}
+  local lns = {}
   local toks = {}
   -- split by lines
   for line in utf8.gmatch(text,"[^\n]+") do
@@ -112,30 +124,32 @@ function tastytxt.tok(text)
     for word in utf8.gmatch(line,"%S+") do
       table.insert(toks,word)
     end
-    table.insert(lines,toks)
+    table.insert(lns,toks)
   end
-  return lines
+  return lns
 end
 
 -- count token occurences and return the token,occurences map
-function tastytxt.tokmap(text)
+function tastytxt.vocab(text)
   local text = text
-  local wordmap = {}
+  local vocab = {}
+  -- tokenize punctuation first
+  text = tastytxt.tokpunct(text)
   -- count tokens
   for word in utf8.gmatch(text,"%S+") do
-    if wordmap[word] then
-      wordmap[word] = wordmap[word] + 1
+    if vocab[word] then
+      vocab[word] = vocab[word] + 1
     else
-      wordmap[word] = 1
+      vocab[word] = 1
     end
   end
-  return wordmap
+  return vocab
 end
 
 -- return tokens occurring <= max number of times
 function tastytxt.rare(text,max)
   -- count tokens
-  local wordmap = tastytxt.tokmap(text)
+  local wordmap = tastytxt.vocab(text)
   -- find singletons
   local singletons = {}
   for k,v in next,wordmap,nil do
@@ -147,18 +161,20 @@ function tastytxt.rare(text,max)
   return singletons
 end
 
--- return vocabulary of text and its cnt
-function tastytxt.vocab(text)
+-- return a set of text tokens
+function tastytxt.unique(text)
   local text = text
-  local vocab = {}
+  local tokset = {}
   local cnt = 0
+  -- tokenize punctuation first
+  text = tastytxt.tokpunct(text)
   for token in utf8.gmatch(text,"%S+") do
-    if vocab[token] == nil then
-      vocab[token] = true
+    if tokset[token] == nil then
+      tokset[token] = true
       cnt = cnt + 1
     end
   end
-  return vocab,cnt
+  return tokset,cnt
 end
 
 -- replace a digit with 0
@@ -232,5 +248,53 @@ function tastytxt.countstrkeys(table)
     end
     return keycnt
 end
+
+-- split text into tokens using separator or whitespace
+function tastytxt.split(text,sep)
+  if sep then
+    assert(type(sep) == "string" and #sep == 1, "Incorrect separator!")
+  end
+
+  local toks = {}
+  -- split into lines first
+  for _,line in ipairs(tastytxt.splitlines(text)) do
+    for word in utf8.gmatch(line,string.format("[^%s]+",sep) or "%S+") do
+      table.insert(toks,word)
+    end
+  end
+  return toks
+end
+
+-- split text into table of lines
+function tastytxt.splitlines(text)
+  local lns = {}
+  -- split into lines
+  for line in utf8.gmatch(text,"[^\n]+") do
+    table.insert(lns,line)
+  end
+  return lns
+end
+
+-- remove only the first sentence from each line of the given text
+function tastytxt.remove_first_sentence(text)
+  local lns = tastytxt.splitlines(text)
+  local new_lns = {}
+  for i=1,#lns do
+    -- notice the whitespace after the dot
+    -- here gsub returns 2 params and overloaded table.insert that accepts 3 values is invoked
+    table.insert(new_lns, (utf8.gsub(lns[i],"^[^%.]+%. ","",1)))
+  end
+  return new_lns
+end
+
+-- count the number of elements in a table
+function tastytxt.tabcnt(tab)
+  local cnt = 0
+  for _ in pairs(tab) do
+    cnt = cnt + 1
+  end
+  return cnt
+end
+
 
 return tastytxt
